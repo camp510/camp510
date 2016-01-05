c6d92375d3f56af99efd204c810a59df516a6a64
John Hattori <johnmhattori@yahoo.com>
2016-01-04 20:16:37 -0800

    add embed video to blog_full

--- js/custom.js
+++ js/custom.js
@@ -37,40 +37,40 @@ $(document).ready(function() {
   $('#map_iframe').mouseleave(function  () {
     $('#map_iframe').addClass('scrolloff');
   });
-});
 
-// Find all YouTube videos
-var $allVideos = $("iframe[src^='//www.vimeo.com']"),
+  // Find all YouTube videos
+  var $allVideos = $("iframe[src^='//www.vimeo.com']"),
 
-    // The element that is fluid width
-    $fluidEl = $("body");
+      // The element that is fluid width
+      $fluidEl = $("body");
 
-// Figure out and save aspect ratio for each video
-$allVideos.each(function() {
-
-  $(this)
-    .data('aspectRatio', this.height / this.width)
-
-    // and remove the hard coded width/height
-    .removeAttr('height')
-    .removeAttr('width');
-
-});
-
-// When the window is resized
-$(window).resize(function() {
-
-  var newWidth = $fluidEl.width();
-
-  // Resize all videos according to their own aspect ratio
+  // Figure out and save aspect ratio for each video
   $allVideos.each(function() {
 
-    var $el = $(this);
-    $el
-      .width(newWidth)
-      .height(newWidth * $el.data('aspectRatio'));
+    $(this)
+      .data('aspectRatio', this.height / this.width)
+
+      // and remove the hard coded width/height
+      .removeAttr('height')
+      .removeAttr('width');
 
   });
 
-// Kick off one resize to fix all videos on page load
-}).resize();
+  // When the window is resized
+  $(window).resize(function() {
+
+    var newWidth = $fluidEl.width();
+
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function() {
+
+      var $el = $(this);
+      $el
+        .width(newWidth)
+        .height(newWidth * $el.data('aspectRatio'));
+
+    });
+
+  // Kick off one resize to fix all videos on page load
+  }).resize(); 
+});
